6c054bfdc38a3310985c36a90b3d554a046259ea
Giacomo-DeGrandi <carlo.de-grandi-giacomo@laplateforme.io>
2021-12-03 10:26:00 +0100

    Last commit pour projet finit

--- index.php
+++ index.php
@@ -4,12 +4,23 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>home</title>
+	<link href="livreor.css" rel="stylesheet">
 </head>
 <body>
-	<h1> Your personal team of web Dev</h1><br>
-	<a href="inscription.php" target="_top">SIGN UP </a>
-	<br><br>
-	<a href="connexion.php" target="_top">LOG IN</a>
-	<br><br><br><br>
+<main>
+	<div id="indextitles">
+		<h1> Hi! Welcome to goldenbook</h1><br>
+		<a href="inscription.php" target="_top">SIGN UP </a> 
+		<br><br>
+		<a href="connexion.php" target="_top">LOG IN</a>
+		<br><br>
+		<a href="livre-or.php" target="_top"> go to the reviews page </a>
+		<br><br><br><br>
+	</div>
+		<footer>
+			<p>giditree<p>
+				<a href="https://github.com/Giacomo-DeGrandi">mon github</a> 
+		</footer>
+</main>
 </body>
 </html>
